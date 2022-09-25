bb8bc3a2319ff5e33d2f21e4ec5eb9f5d1374abd
rudzzz <rudsolza13@gmail.com>
2022-09-25 11:16:27 -0300

    files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("---------Temperature Converter---------");
+		System.out.println("1 - Celsius to Fahrenheit");
+		System.out.println("2 - Fahrenheit to Celsius");
+	}
+
+}
